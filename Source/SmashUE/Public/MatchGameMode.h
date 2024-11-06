b77638165192eed7884e29192fd4e2f893150d62
Sacha <S.Soulslasher@gmail.com>
2024-11-06 09:10:18 +0100

    create MatchGameMode

---
+++ Source/SmashUE/Public/MatchGameMode.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MatchGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHUE_API AMatchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
