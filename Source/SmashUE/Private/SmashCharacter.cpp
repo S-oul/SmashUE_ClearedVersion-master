42804d36e2be3a7ad623a706e8ca288065d3dd7a
Sacha (Pc) <S.soulslasher@gmail.com>
2024-11-06 15:42:12 +0100

    Commit 1-53/64

--- Source/SmashUE/Private/SmashCharacter.cpp
+++ Source/SmashUE/Private/SmashCharacter.cpp
@@ -23,6 +23,7 @@ void ASmashCharacter::BeginPlay()
 void ASmashCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	RotateMeshUsingOrientX();
 
 }
 
@@ -31,6 +32,23 @@ void ASmashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+float ASmashCharacter::GetOrientX() const
+{
+	return OrientX;
+}
+
+void ASmashCharacter::SetOrientX(float NewOrientX)
+{
+	OrientX = NewOrientX;
+}
+
+void ASmashCharacter::RotateMeshUsingOrientX() const
+{
+	FRotator Rotation = GetMesh()->GetRelativeRotation();
+	Rotation.Yaw = -90.0f * OrientX;
+	GetMesh()->SetRelativeRotation(Rotation);
 }
 
 
